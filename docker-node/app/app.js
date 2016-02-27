eaffe4598c6e7182c1098663d7c6b8853a8e5dda
Stephane Erbrech <stephane.erbrech@gmail.com>
2016-02-27 02:50:46 +0100

    compose load balancer

--- docker-node/app/app.js
+++ docker-node/app/app.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var fs = require('fs');
+var os = require('os');
 var app = express();
 var port = 5001;
 
 app.get('/', function (req, res) {
   res.send('<h1>Hello GDG STBG!</h1>');
+});
+
+app.get('/ip', function (req, res) {
+  var interfaces = os.networkInterfaces();
+  var addresses = [];
+  for (var k in interfaces) {
+      for (var k2 in interfaces[k]) {
+          var address = interfaces[k][k2];
+          if (address.family === 'IPv4' && !address.internal) {
+              addresses.push(address.address);
+          }
+      }
+  }
+  res.send('<h1>' + addresses.toString() + '</h1>');
 });
 
 app.listen(port, function () {
